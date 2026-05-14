094a29680dbd272f7fd9ce87fad1f05f74c4201d
Yangshu233 <750704076@qq.com>
2026-05-14 16:17:42 +0800

    chore: adjust repo structure

--- src/plugins/lazy_monitor/__init__.py
+++
@@ -1,7 +0,0 @@
-"""向后兼容 shim。
-
-通过 `load_plugins("src/plugins")` 加载时自动转发到 nonebot_plugin_lazy。
-新项目推荐直接使用 `nonebot.load_plugin("nonebot_plugin_lazy")`。
-"""
-
-from nonebot_plugin_lazy import *  # noqa: F401,F403
